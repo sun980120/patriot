915172b8ba5f5542413ac2f0e98e7fc5978eb06d
WooSungHong <ws.hong@intin.kr>
2026-05-21 15:39:02 +0900

    add web app manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { CLUB_NAME } from '@/lib/constants';
 export const metadata: Metadata = {
   title: `${CLUB_NAME} 스마트 회비 관리 웹 애플리케이션`,
   description: `${CLUB_NAME} 동호회 회비, 세입, 지출, 회원 승인 관리를 위한 웹 애플리케이션`,
+  manifest: '/manifest.json',
   icons: {
     icon: '/favicon.ico',
     apple: '/apple-touch-icon.png',
